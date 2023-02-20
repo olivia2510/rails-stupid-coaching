5a4380e089bfdb111824b104dba6bf4cf51c129d
Olivia Marien <olivia.jr.marien@gmail.com>
2023-02-20 15:14:56 +0100

    Stupid Coaching

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,10 @@
+class AnswersController < ApplicationController
+end
+
+def answer
+  params[:question]
+end
+
+# If the message is I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise the coach will answer I don't care, get dressed and go to work!
